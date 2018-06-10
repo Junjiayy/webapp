7d6d9e1cd5f1b517a885c19c5ba603ea0fb8c656
张俊加 <1020446694@qq.com>
2018-06-10 15:28:28 +0800

    添加未读消息

---
+++ src/mixins/unreadCount.js
@@ -0,0 +1,36 @@
+import wepy from 'wepy'
+import api from '@/utils/api'
+
+export default class unreadCount extends wepy.mixin {
+  data = {
+    interval: null,
+    unreadCount: 0
+  }
+
+  onShow() {
+    this.updateUnreadCount()
+    this.interval = setInterval(() => {
+      this.updateUnreadCount()
+    }, 30000)
+  }
+
+  onHide() {
+    clearInterval(this.interval)
+  }
+
+  updateUnreadCount() {
+    this.unreadCount = this.$parent.globalData.unreadCount
+    this.$apply()
+
+    if (this.unreadCount) {
+      wepy.setTabBarBadge({
+        index: 1,
+        text: this.unreadCount.toString()
+      })
+    } else {
+      wepy.removeTabBarBadge({
+        index: 1
+      })
+    }
+  }
+}
